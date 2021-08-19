fa12c43e8cb01b28b586b6f356d4abb11b626df9
enjoyprograming <1129893614@qq.com>
2021-08-19 11:33:09 +0800

    单例模式代码提交

---
+++ 001_Singleton/Singleton2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Singleton
+{
+    /// <summary>
+    /// 2.安全的线程
+    /// </summary>
+    public sealed class Singleton2
+    {
+        static Singleton2 instance = null;
+        static readonly object padlock = new object();
+        Singleton2()
+        {
+
+        }
+
+        public static Singleton2 Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton2();
+                    }
+                    return instance;
+                }
+            }
+        }
+        //这种方式的实现对于线程来说是安全的。我们首先创建了一个进程辅助对象，线程在进入时先对辅助对象加锁然后再检测对
+        //象是否被创建，这样可以确保只有一个实例被创建，因为在同一个时刻加了锁的那部分程序只有一个线程可以进入。这种情
+        //况下，对象实例由最先进入的那个线程创建，后来的线程在进入时（instence == null）为假，不会再去创建对象实例了。
+        //但是这种实现方式增加了额外的开销，损失了性能
+
+    }
+}
---
+++ 01_Singleton/Singleton2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Singleton
+{
+    /// <summary>
+    ///  单例模式（常规用法，线程安全）
+    /// </summary>
+    public class Singleton2
+    {
+        /// <summary>
+        /// 私有构造函数
+        /// </summary>
+        private Singleton2()
+        {
+
+        }
+        /// <summary>
+        /// 静态局部变量
+        /// </summary>
+        private static Singleton2 _instance = null;
+        /// <summary>
+        /// 声明锁 锁同步
+        /// </summary>
+        private static readonly object _lock = new object();
+
+        /// <summary>
+        /// 静态的全局唯一访问口
+        /// 只能得到缓存的静态局部变量实例，无法重新新建
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Singleton2 GetInstance(string value)
+        {
+            if (_instance==null)//双重判空，确保在多线程状态下只创建一个实例对象
+            {
+                lock (_lock)//加锁，确保其每次只能由一个对象进行访问
+                {
+                    if (_instance==null)
+                    {
+                        _instance = new Singleton2();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string Value { get; set; }
+
+        //懒汉模式二（线程安全）
+    }
+}
